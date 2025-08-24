79f29cd1a799528e2da4ffbde6e48f2b9664126a
404NotFound1011 <topaz5487@proton.me>
2025-08-24 15:25:38 +0100

    Update 20251019.png.py

--- api/20251019.png.py
+++ api/20251019.png.py
@@ -28,7 +28,7 @@ config = {
 
     "message": { # Show a custom message when the user opens the image
         "doMessage": True, # Enable the custom message?
-        "message": "This browser has been pwned by Dexty's Image Logger. https://github.com/xdexty0/Discord-Image-Logger", # Message to show
+        "message": "GET FUCKING PWNED LOSER", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
